272420156f0d1e0ef11972ed65e13473d84c4411
littlefean <2028140990@qq.com>
2023-12-17 16:27:12 +0800

    init project

---
+++ 08 closure.js
@@ -0,0 +1,20 @@
+function f() {
+    let a = 5;
+
+    function g() {
+        console.log(a);
+        a++;
+    }
+
+    return g;
+}
+
+let g1 = f();
+let g2 = f();
+
+g1();
+g1();
+g1();
+
+g2();
+g2();
---
+++ 迷惑行为.js
@@ -0,0 +1,43 @@
+// let bigArr = [];
+// let number = 100;
+//
+// for (let i = 0; i < number; i++) {
+//     bigArr.push([]);
+// }
+//
+// for (let i = 0; i < number - 1; i++) {
+//     bigArr[i].push(bigArr[i + 1])
+// }
+//
+// bigArr[number - 1].push(bigArr[0]);
+//
+// delete bigArr;
+// console.log(bigArr);
+
+// let o = {};
+// o.o = o;
+// console.log(o.o.o.o.o.o.o);
+
+function f() {
+    console.log("123132");
+    return f;
+}
+
+// let string = "f";
+// for (let i = 0; i < 100000; i++) {
+//     string += "()";
+// }
+// eval(string);
+
+let range = function* (n) {
+    for (let i = 0; i < n; i++) {
+        yield i;
+    }
+}
+
+let print = console.log;
+
+for (let i of range(5))                       {
+    print(i)                                    ;
+                                                }
+
